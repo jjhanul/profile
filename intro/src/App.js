3ecfcb83082160f9563849c94a7db7cca6aea096
hanul <seol194@naver.com>
2023-08-13 21:45:15 +0900

    컴포넌트 추가

---
+++ intro/src/App.js
@@ -0,0 +1,12 @@
+import InfoList from './component/InfoList';
+import Name from '/component/Name';
+
+function App() {
+  return (
+    <div className="App">
+      <Name />
+      <InfoList/>
+    </div>
+  );
+}
+export default App;
